1f5440bde29be8f5f59e93e65b2a64a8839a836e
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-01-25 22:45:10 +0100

    Added front (in progress)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+
+
 function App() {
+  const [todos, setTodos] = useState([{ text: 'walk dog', completed: false }]);
+  const [todo, setTodo] = useState('');
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    if (todo != '') {
+      setTodos([todo, ...todos]);
+      setTodo('');
+    }
+    e.preventDefault();
+  }
+
+  const Todo = (todo) => {
+    return (
+      <div className={"todo"}>
+        <p className="todo-name">{todo.text}</p>
+        <input className="todo-checkbox" type="checkbox" />
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="todos">
+          {todos.map(todo => <Todo todo={todo} />)}
+        </div>
+        <form className="add" onSubmit={handleSubmit} >
+          <input className="todo-input" value={todo} onChange={handleChange} placeholder="Add new todo" />
+          <button className="todo-button">Add</button>
+        </form>
+
+      </div>
     </div>
   );
 }
